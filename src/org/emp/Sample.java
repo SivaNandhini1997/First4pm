b569b69deecf3cc15c112ec3306a3bcf2be73849
SivaNandhini1997 <nandynandhukannan@gmail.com>
2022-08-30 14:53:00 +0530

    Codes added by chand

--- src/org/emp/Sample.java
+++ src/org/emp/Sample.java
@@ -2,6 +2,11 @@ package org.emp;
 
 public class Sample {
 	
+	
+	public void m2() {
+		System.out.println("Chand added his code");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Successfull...........");
