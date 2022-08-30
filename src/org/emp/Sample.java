0f79ac6655bfc5aee6634447aef331f4ed78c385
SivaNandhini1997 <nandynandhukannan@gmail.com>
2022-08-30 14:33:07 +0530

    Codes added

---
+++ src/org/emp/Sample.java
@@ -0,0 +1,14 @@
+package org.emp;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Successfull...........");
+		System.out.println("Siva...........");
+		
+		
+	}
+	
+
+}
